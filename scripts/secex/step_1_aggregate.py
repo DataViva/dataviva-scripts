1bfb3a49943ab61c9c605b7a5bb968fa98e129cb
Mario Mol <mariohmol@gmail.com>
2014-06-16 14:48:01 -0300

    step 0 to secex scripts: check

--- scripts/secex/step_1_aggregate.py
+++ scripts/secex/step_1_aggregate.py
@@ -79,38 +79,6 @@ def add(ybpw, munic, isic, occ, val_usd):
     ybpw[munic][isic][occ]["val_usd"] += val_usd
     return ybpw
 
-def updateMDICxIBGE():
-    '''Open CSV file'''
-    raw_file_path = os.path.abspath(os.path.join(DATA_DIR, 'secex', 'MDICxIBGE.csv'))
-    raw_file = get_file(raw_file_path)
-    delim = ";"
-    csv_reader = csv.reader(raw_file, delimiter=delim)
-    for i, line in enumerate(csv_reader):
-        #7 = IBGE , 9 = MDIC
-        ibge_cod=line[7].strip()
-        mdic_cod=line[9].strip()
-        if i==0:
-            continue
-
-        sql="select id_mdic, id from attrs_bra where id_ibge = {0}".format(ibge_cod)
-        cursor.execute(sql)
-        values=cursor.fetchall()
-        size=len(values)
-        if size==1:          
-            old_mdic=str(values[0][0])
-            old_idbra=str(values[0][1])
-            if old_mdic<>mdic_cod:
-                print "Changing MDIC code from {0} to {1} in IBGE cod {2}".format(old_mdic,mdic_cod,ibge_cod)
-                sql="select * from secex_yb where bra_id='{0}'".format(old_idbra)
-                cursor.execute(sql)
-                if len(cursor.fetchall())>0:
-                    print "Exports found for {0}".format(ibge_cod)
-                sql="update attrs_bra set id_mdic = {0} where id_ibge = {1}".format(mdic_cod,ibge_cod)
-                #cursor.execute(sql)
-        else:
-            print "Error finding a IBGE {0}: Found {1}".format(ibge_cod,size)
-
-
 def main(year):
     '''Initialize our data dictionaries'''
     ybpw = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float))))
