c92345aa154478923817ca438c022e02901e0788
Alex Simoes <alexandersimoes@gmail.com>
2014-07-31 11:22:59 -0400

    new format for dani's search

--- scripts/misc/builds_to_json.py
+++ scripts/misc/builds_to_json.py
@@ -8,32 +8,40 @@ db = MySQLdb.connect(host="localhost", user=os.environ["DATAVIVA_DB_USER"],
 db.autocommit(1)
 cursor = db.cursor()
 
-data = []
+output = []
 cursor.execute("select title_en, name_en, filter1, filter2 from apps_build, apps_app where app_id=apps_app.id order by title_en")
-last = {"build_title":None}
-for r in cursor.fetchall():
-    # if last is None:
-    #     last = {
-    #         "build_title": r[0],
-    #         "apps": [r[1]],
-    #         "filters": set(["bra"])
-    #     } 
-    if r[0] != last["build_title"]:
-        if last["build_title"] is not None:
-            last["filters"] = list(last["filters"])
-            data.append(last)
-        last = {
-            "build_title": r[0],
-            "apps": [],
-            "filters": set(["bra"])
-        }
-    last["apps"].append(r[1])
-    if "<" in r[2]:
-        last["filters"].add(r[2].replace(">", "").replace("<", ""))
-    if "<" in r[3]:
-        last["filters"].add(r[3].replace(">", "").replace("<", ""))
 
-j = json.dumps(data, indent=4)
-f = open('dataviva_builds.json', 'w')
+builds = defaultdict(list)
+for r in cursor.fetchall():
+    builds[r[0]].append(r)
+for b_name, b in builds.items():
+    data = {
+        "build_title": b_name, 
+        "apps": [], 
+        "filters": set(["bra"])
+    }
+    for bb in b:
+        if "Compare" in bb[1]:
+            compare_build = {
+                "build_title": b_name.replace("<bra>", "<bra> and <bra>"), 
+                "apps": [bb[1]], 
+                "filters": ["bra", "bra"]
+            }
+            if "<" in bb[2]:
+                compare_build["filters"].append(bb[2].replace(">", "").replace("<", ""))
+            if "<" in bb[3]:
+                compare_build["filters"].append(bb[3].replace(">", "").replace("<", ""))
+            output.append(compare_build)
+        else:
+            data["apps"].append(bb[1])
+            if "<" in bb[2]:
+                data["filters"].add(bb[2].replace(">", "").replace("<", ""))
+            if "<" in bb[3]:
+                data["filters"].add(bb[3].replace(">", "").replace("<", ""))
+    data["filters"] = list(data["filters"])
+    output.append(data)
+
+j = json.dumps(output, indent=4)
+f = open('dataviva_builds_new.json', 'w')
 print >> f, j
 f.close()
