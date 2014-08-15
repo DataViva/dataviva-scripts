d464a153999937924a77861d532e7a8cc64808bd
Alex Simoes <alexandersimoes@gmail.com>
2014-08-15 17:16:15 -0400

    attrs for search updated with new datasets

--- scripts/misc/attrs_to_json.py
+++ scripts/misc/attrs_to_json.py
@@ -12,9 +12,6 @@ data = {}
 cursor.execute("select cbo_id, name_en, avg(num_emp) from rais_yo, attrs_cbo where id=cbo_id group by cbo_id;")
 data["cbo"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
 
-cursor.execute("select isic_id, name_en, avg(num_emp) from rais_yi, attrs_isic where id=isic_id group by isic_id;")
-data["isic"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
-
 cursor.execute("select bra_id, name_en, avg(population) from attrs_yb, attrs_bra where id=bra_id group by bra_id")
 data["bra"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
 
@@ -23,6 +20,31 @@ data["hs"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fet
 
 cursor.execute("select wld_id, name_en, avg(val_usd) from secex_yw, attrs_wld where id=wld_id group by wld_id;")
 data["wld"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
+db = MySQLdb.connect(host="localhost", user=os.environ["DATAVIVA2_DB_USER"], 
+                        passwd=os.environ["DATAVIVA2_DB_PW"], 
+                        db=os.environ["DATAVIVA2_DB_NAME"])
+db.autocommit(1)
+cursor = db.cursor()
+
+cursor.execute("select cnae_id, name_en, avg(num_emp) from rais_yi, attrs_cnae where id=cnae_id group by cnae_id;")
+data["cnae"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
+cursor.execute("select inst_id, name_en, sum(people) from lattes_bri, attrs_inst where id=inst_id group by inst_id having sum(people) >= 15")
+data["inst"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
+cursor.execute("select research_id, name_en, sum(people) from lattes_bri, attrs_research where id=research_id group by research_id")
+data["research"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
+cursor.execute("select grade, name_en, sum(enrolled) from edu_ybge, attrs_grade where id=grade and year = 2012 group by grade")
+data["grade"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
+cursor.execute("select ethnicity, name_en, sum(enrolled) from edu_ybge, attrs_ethnicity where id=ethnicity and year = 2012 group by ethnicity")
+data["ethnicity"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
+cursor.execute("select course_id, name_en, sum(enrolled) from hedu_ybuc, attrs_course where id=course_id and year = 2012 group by course_id")
+data["course"] = [{"id":x[0], "name":x[1], "weight":float(x[2])} for x in cursor.fetchall()]
+
 
 j = json.dumps(data, indent=4)
 f = open('dataviva_attrs.json', 'w')
