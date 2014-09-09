8eff9d5b55208d38294c53e3017092056a8f42d9
Alex Simoes <alexandersimoes@gmail.com>
2014-09-09 17:18:33 -0400

    resolved error in shard script

--- scripts/rais_new/format_raw_data.py
+++ scripts/rais_new/format_raw_data.py
@@ -41,44 +41,33 @@ from _column_lengths import add_column_length
 @click.option('output_path', '--output', '-o', help='Path to save files to.', type=click.Path(), required=True, prompt="Output path")
 @click.option('prev_path', '--prev', '-g', help='Path to files from the previous year for calculating growth.', type=click.Path(exists=True), required=False)
 @click.option('prev5_path', '--prev5', '-g5', help='Path to files from 5 years ago for calculating growth.', type=click.Path(exists=True), required=False)
-@click.option('-d', '--debug', is_flag=True)
-def main(file_path, year, output_path, prev_path, prev5_path, debug):
+def main(file_path, year, output_path, prev_path, prev5_path):
     start = time.time()
     step = 0
     
-    # d = pd.HDFStore(os.path.abspath(os.path.join(output_path,'rais.h5')))
-    # if "ybio" in d:
-    if 1 ==2:
+    d = pd.HDFStore(os.path.abspath(os.path.join(output_path,'rais.h5')))
+    if "ybio" in d:
         tables = {}
         tables["yb"] = d["yb"]; tables["yi"] = d["yi"]; tables["yo"] = d["yo"]; tables["ybi"] = d["ybi"]; tables["ybo"] = d["ybo"]; tables["yio"] = d["yio"]; tables["ybio"] = d["ybio"]
     else:
-        if debug:
-            step+=1; print; print '''STEP {0}: \nImport file to pandas dataframe'''.format(step)
-        rais_df = to_df(file_path, False, debug)
+        step+=1; print; print '''STEP {0}: \nImport file to pandas dataframe'''.format(step)
+        rais_df = to_df(file_path, False)
 
-        if debug:
-            step+=1; print; print '''STEP {0}: \nAggregate'''.format(step)
+        step+=1; print; print '''STEP {0}: \nAggregate'''.format(step)
         ybio = aggregate(rais_df)
 
-        if debug:
-            step+=1; print; print '''STEP {0}: \nShard'''.format(step)
+        step+=1; print; print '''STEP {0}: \nShard'''.format(step)
         tables = shard(ybio, rais_df)
         
         # sys.exit()
         
         # for t_name, t in tables.items():
         #     d[t_name] = t
-        
-    # if debug:
-    #     step+=1; print; print '''STEP {0}: \nRequired'''.format(step)
-    # ybio = required(ybio, ybi, yi, year)
     
-    if debug:
-        step+=1; print; print 'STEP {0}: \nImportance'.format(step)
+    step+=1; print; print 'STEP {0}: \nImportance'.format(step)
     yio = importance(tables["ybio"], tables["ybi"], tables["yio"], tables["yo"], year)
 
-    if debug:
-        step+=1; print; print 'STEP {0}: \nDiversity'.format(step)
+    step+=1; print; print 'STEP {0}: \nDiversity'.format(step)
     yb = calc_diversity(tables["ybi"], tables["yb"], "bra_id", "cnae_id", year)
     yb = calc_diversity(tables["ybo"], tables["yb"], "bra_id", "cbo_id", year)
     yi = calc_diversity(tables["ybi"], tables["yi"], "cnae_id", "bra_id", year)
@@ -86,8 +75,7 @@ def main(file_path, year, output_path, prev_path, prev5_path, debug):
     yo = calc_diversity(tables["ybo"], tables["yo"], "cbo_id", "bra_id", year)
     yo = calc_diversity(tables["yio"], tables["yo"], "cbo_id", "cnae_id", year)
 
-    if debug:
-        step+=1; print; print 'STEP {0}: \nCalculate RCA, diversity and opportunity gain aka RDO'.format(step)
+    step+=1; print; print 'STEP {0}: \nCalculate RCA, diversity and opportunity gain aka RDO'.format(step)
     ybi = rdo(tables["ybi"], tables["yi"], year)
 
     for table_name, table_data in tables.items():
@@ -95,8 +83,7 @@ def main(file_path, year, output_path, prev_path, prev5_path, debug):
         # print table_data.head()
         
     if prev_path:
-        if debug:
-            step+=1; print; print '''STEP {0}: \nCalculate 1 year growth'''.format(step)
+        step+=1; print; print '''STEP {0}: \nCalculate 1 year growth'''.format(step)
         for t_name, t in tables.items():
             prev_file = os.path.join(prev_path, "{0}.tsv.bz2".format(t_name))
             t_prev = to_df(prev_file, t_name)
@@ -110,8 +97,7 @@ def main(file_path, year, output_path, prev_path, prev5_path, debug):
             # sys.exit()
             
             if prev5_path:
-                if debug:
-                    step+=1; print; print '''STEP {0}: \nCalculate 5 year growth'''.format(step)
+                step+=1; print; print '''STEP {0}: \nCalculate 5 year growth'''.format(step)
                 prev_file = os.path.join(prev5_path, "{0}.tsv.bz2".format(t_name))
                 t_prev = to_df(prev_file, t_name)
                 t_prev = t_prev.reset_index(level="year")
@@ -123,8 +109,7 @@ def main(file_path, year, output_path, prev_path, prev5_path, debug):
                 t = calc_growth(t, t_prev, 5)
     
     
-    if debug:
-        print; print '''FINAL STEP: \nSave files to output path'''
+    print; print '''FINAL STEP: \nSave files to output path'''
     for t_name, t in tables.items():
         if not os.path.exists(output_path):
             os.makedirs(output_path)
