15cf8c0effc55506f7195fb868ca468ec588e010
Jonathan Speiser <jspeis@gmail.com>
2014-09-17 10:39:45 -0400

    Adding back age_f calculation

--- scripts/sc/_aggregate.py
+++ scripts/sc/_aggregate.py
@@ -2,7 +2,8 @@ import sys
 import pandas as pd
 import numpy as np
 
-URBAN= 1
+URBAN = 1
+FEMALE = 1
 
 def aggregate(df):
     # split by gender
@@ -12,10 +13,12 @@ def aggregate(df):
     
     df["enrolled_urban"] = 0
     df["enrolled_urban"][df["loc"] == URBAN] = 1
+    df["age_f"] = 0
+    df["age_f"][df["gender"] == FEMALE] = df['age']
 
-    df_f = df[df["gender"]==1]
+    df_f = df[df["gender"]==FEMALE]
         
-    df = df.groupby(["year", "bra_id", "grade", "ethnicity"]).agg({"schools": pd.Series.nunique, "classes": pd.Series.nunique, "enrolled": pd.Series.nunique, "age":np.sum, "enrolled_urban":np.sum})
+    df = df.groupby(["year", "bra_id", "grade", "ethnicity"]).agg({"schools": pd.Series.nunique, "classes": pd.Series.nunique, "enrolled": pd.Series.nunique, "age":np.sum,"age_f":np.sum, "enrolled_urban":np.sum})
     df_f = df_f.groupby(["year", "bra_id", "grade", "ethnicity"]).agg({"enrolled": pd.Series.nunique})
     df_f = df_f.rename(columns={"enrolled":"enrolled_f"})
     
@@ -31,5 +34,6 @@ def aggregate(df):
     ybge = pd.concat([ybge, ybge_state])
     
     ybge["age"] = ybge["age"] / ybge["enrolled"]
-
+    ybge["age_f"] = ybge["age_f"] / ybge["enrolled_f"]
+    
     return ybge
