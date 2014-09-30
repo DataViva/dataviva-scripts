97232d969e468b8e43f98d03d9820adf42f0590d
Jon Speiser <jspeis@gmail.com>
2014-09-30 16:13:49 -0400

    renaming cols

--- scripts/rais_new/_growth.py
+++ scripts/rais_new/_growth.py
@@ -10,7 +10,7 @@ def calc_growth(tbl, tbl_prev, years_ago=1):
         col_name = "{0}_{1}".format(col_name, years_ago)
     tbl[col_name] = tbl["wage"] - tbl_prev["wage"]
     
-    col_name = "num_emp_val"
+    col_name = "num_emp_growth_val"
     if years_ago > 1:
         col_name = "{0}_{1}".format(col_name, years_ago)
     tbl[col_name] = tbl["num_emp"] - tbl_prev["num_emp"]
@@ -19,12 +19,12 @@ def calc_growth(tbl, tbl_prev, years_ago=1):
     # print tbl.index.is_unique
 
     '''Growth rate'''
-    col_name = "wage_growth_rate"
+    col_name = "wage_growth_pct"
     if years_ago > 1:
         col_name = "{0}_{1}".format(col_name, years_ago)
     tbl[col_name] = (tbl["wage"] / tbl_prev["wage"]) ** (1.0/years_ago) - 1
     
-    col_name = "num_emp_rate"
+    col_name = "num_emp_growth_pct"
     if years_ago > 1:
         col_name = "{0}_{1}".format(col_name, years_ago)
     tbl[col_name] = (tbl["num_emp"] / tbl_prev["num_emp"]) ** (1.0/years_ago) - 1
