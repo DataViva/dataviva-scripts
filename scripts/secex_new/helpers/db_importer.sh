8c6a2631c8ae309e34731d193594ff62ef89946b
Alex Simoes <alexandersimoes@gmail.com>
2014-10-01 15:00:45 -0400

    helper file for importing data to DB

---
+++ scripts/secex_new/helpers/db_importer.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+MODE="secex"
+
+if [ -z "$1" ]
+  then
+    echo "** ERROR: No directory supplied. Please path an absolute folder path.";
+    exit -1;
+fi
+
+
+# -- one parameter required absolute path to folder
+FOLDER=$1
+
+for fullpath in $FOLDER/*.tsv.bz2
+do
+    echo "Full path: $fullpath";
+    filename=$(basename $fullpath)
+    tsvname="${filename%.bz2}";
+    
+    # -- Unzip
+    echo "Unzipping $fullpath ..."
+    bzcat $fullpath > $FOLDER/$tsvname
+
+    tsvpath="$FOLDER/$tsvname"
+
+    # -- Import to database
+    tablename="${MODE}_${filename%.tsv.bz2}";
+    echo "Importing $tsvpath to SQL table $tablename";
+
+    # -- Take the first line of the tsv and convert tabs to commas so we can map tsv fields -> mysql columns
+    # fields=`head -1 $tsvpath | tr '\t' ','`
+    # fields='nullif(@v'
+    # fields+=`head -1 $tsvpath | sed 's/  /,""),nullif(@v/g'`
+    # fields='nullif(@v$fields,"")'
+    # fields+=',"")'
+    sql_fields=""
+    sql_set=""
+    fields=`head -1 $tsvpath | tr '\t' ' '`
+    for field in ${fields[@]}
+    do
+      sql_fields+="@v$field, "
+      sql_set+="$field = nullif(@v$field,''), "
+    done
+    sql_set=${sql_set%", "}
+    sql_fields=${sql_fields%", "}
+    # echo $sql_fields
+    # echo $sql_set
+    # echo $fields;
+    mysql -uroot $DATAVIVA2_DB_NAME -e "LOAD DATA LOCAL INFILE '$tsvpath' INTO TABLE $tablename FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES  ($sql_fields) SET $sql_set;";
+
+    # -- Clean up tmp file
+    rm $FOLDER/$tsvname;
+    echo "Completed import to $tablename";
+
+done
+ 
+
+
