1ae24128333ccdfe7e9c34edd8d569c159d60c69
Jon Speiser <jspeis@gmail.com>
2014-11-26 20:06:49 -0500

    bugfix for file order and cleanup

--- scripts/common/growth_calc.py
+++ scripts/common/growth_calc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    python scripts/common/growth_calc.py file.tsv.bz2 file2.tsv.bz2 --years=1 --cols=wage,age -o /tmp
+    python scripts/common/growth_calc.py file_2007.tsv.bz2 file_2008.tsv.bz2 --years=1 --cols=wage,age -o /tmp
 
 """
 
@@ -23,7 +23,6 @@ def parse_table_name(t):
     return m.group(1)
 
 def do_growth(tbl, tbl_prev, cols, years_ago=1):
-
     '''Growth rate'''
     for orig_col_name in cols:
         print tbl.columns,  tbl_prev.columns
@@ -35,30 +34,29 @@ def do_growth(tbl, tbl_prev, cols, years_ago=1):
     return tbl
 
 @click.command()
-@click.argument('orig_path_str', type=click.Path(exists=True))
-@click.argument('new_path_str', type=click.Path(exists=True))
+@click.argument('first_year_str', type=click.Path(exists=True))
+@click.argument('second_year_str', type=click.Path(exists=True))
 @click.option('-c', '--cols', prompt='Columns separated by commas to compute growth', type=str, required=True)
 @click.option('-y', '--years', prompt='years between data points', type=int, required=False)
 @click.option('output_path', '--output', '-o', help='Path to save files to.', type=click.Path(), required=True, prompt="Output path")
-def main(orig_path_str, new_path_str, cols, output_path, years=1):
+def main(first_year_str, second_year_str, cols, output_path, years=1):
     start = time.time()
     step = 0
     
     step+=1; print; print '''STEP {0}: \nCalculate 1 year growth'''.format(step)
     
-    orig_path = get_file(orig_path_str)
+    orig_path = get_file(first_year_str)
     df1 = pd.read_csv(orig_path, sep="\t")
-    new_path = get_file(new_path_str)
+    new_path = get_file(second_year_str)
     df2 = pd.read_csv(new_path, sep="\t")
 
     col_names = cols.split(",")
     print "CALCULATING growth for the following columns:", col_names
 
-    df2 = do_growth(df1, df2, col_names, years)
+    df2 = do_growth(df2, df1, col_names, years)
     
-    output_path = "/tmp" # - for testing only
-    print output_path
-    t_name = parse_table_name(new_path_str) # str(os.path.dirname(os.path.realpath( new_path_str ))).split("_")[0]
+    
+    t_name = parse_table_name(second_year_str) 
     print "GOT TABLE NAME OF ", t_name
     if not t_name:
         t_name = "noname"
