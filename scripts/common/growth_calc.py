16c3586d3cc6389552c7443179c33978941e7730
Zeus <zeus@Zeuss-Mac-Pro.local>
2014-12-01 15:02:50 -0500

    updating growth util

--- scripts/common/growth_calc.py
+++ scripts/common/growth_calc.py
@@ -16,21 +16,41 @@ utils_path = os.path.abspath(os.path.join(file_path, ".."))
 sys.path.insert(0, utils_path)
 from helpers import get_file
 
+pattern = re.compile('(\w+).tsv(.bz2)*')
+pattern1 = re.compile('(\w+)_(gender|color|loc|school_type|cid2|ethnicity).tsv(.bz2)*')
 
 def parse_table_name(t):
-    pattern = re.compile('(\w+)(_\w+)*.tsv(.bz2)*')
+    t = t.replace('_with_growth', '')
+    m = pattern1.search(t)
+    if m:
+        return m.group(1)
     m = pattern.search(t)
-    return m.group(1)
-
-def do_growth(tbl, tbl_prev, cols, years_ago=1):
+    if m:
+        return m.group(1)
+         
+def do_growth(t_name, tbl, tbl_prev, cols, years_ago=1, edu_mode='hedu'):
     '''Growth rate'''
+    print "COLS", cols
+    pk_lookup = {"b": "bra_id", "u": "university_id", "d": "d_id", "c": "course_%s_id" % (edu_mode) }
+    
+    pk = [pk_lookup[letter] for letter in t_name if letter != 'y']
+
+    tbl = pd.merge(tbl, tbl_prev[pk + cols], how='left', left_on=pk, right_on=pk )
+    
     for orig_col_name in cols:
+        print "DOING", orig_col_name
         print tbl.columns,  tbl_prev.columns
+
         new_col_name = orig_col_name + "_growth"
         if years_ago > 1:
             new_col_name = "{0}_{1}".format(new_col_name, years_ago)
-        tbl[new_col_name] = (tbl[orig_col_name] / tbl_prev[orig_col_name]) ** (1.0/years_ago) - 1
-    
+            
+            
+        tbl[new_col_name] = ((1.0 * tbl[orig_col_name+"_x"]) / (1.0*tbl[orig_col_name + "_y"])) ** (1.0/years_ago) - 1
+    for colname in cols:
+        del tbl[colname + "_y"]
+        tbl.rename(columns={ c+"_x" : c for c in cols }, inplace=True)
+        # tbl[new_col_name] = tbl3[new_col_name] # -- this is safe because of the left merge
     return tbl
 
 @click.command()
@@ -38,8 +58,9 @@ def do_growth(tbl, tbl_prev, cols, years_ago=1):
 @click.argument('second_year_str', type=click.Path(exists=True))
 @click.option('-c', '--cols', prompt='Columns separated by commas to compute growth', type=str, required=True)
 @click.option('-y', '--years', prompt='years between data points', type=int, required=False)
+@click.option('-e', '--edu', prompt='hedu or sc', type=str, required=True)
 @click.option('output_path', '--output', '-o', help='Path to save files to.', type=click.Path(), required=True, prompt="Output path")
-def main(first_year_str, second_year_str, cols, output_path, years=1):
+def main(first_year_str, second_year_str, cols, output_path, edu, years=1):
     start = time.time()
     step = 0
     
@@ -52,16 +73,16 @@ def main(first_year_str, second_year_str, cols, output_path, years=1):
 
     col_names = cols.split(",")
     print "CALCULATING growth for the following columns:", col_names
-
-    df2 = do_growth(df2, df1, col_names, years)
-    
-    
     t_name = parse_table_name(second_year_str) 
+
+    df2 = do_growth(t_name, df2, df1, col_names, years, edu)
+    
+    
     print "GOT TABLE NAME OF ", t_name
     if not t_name:
         t_name = "noname"
     new_file_path = os.path.abspath(os.path.join(output_path, "{0}_with_growth.tsv.bz2".format(t_name)))
-    df2.to_csv(bz2.BZ2File(new_file_path, 'wb'), sep="\t", index=True, float_format="%.2f")
+    df2.to_csv(bz2.BZ2File(new_file_path, 'wb'), sep="\t", index=False, float_format="%.2f")
     
     print("--- %s minutes ---" % str((time.time() - start)/60))
 
