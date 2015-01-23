c7277e4d6e5e21b4bfe1d90da2511ab70a2d92d2
Jon Speiser <jspeis@gmail.com>
2015-01-23 10:06:48 -0500

    updates for col operations

--- scripts/ei/_column_lengths.py
+++ scripts/ei/_column_lengths.py
@@ -1,8 +1,11 @@
+# helper_columns.py
 import pandas as pd
-def add_column_length(table_name, table_data):
-	columns = {'b': 'bra_id', 'c': 'course_hedu_id'}
-	for index, column in columns.items():
-		if index in table_name:
-			table_data[column + "_len"] = pd.Series( map(lambda x: len(str(x)), table_data.index.get_level_values(column)), index = table_data.index)
 
-	return table_data
+def add_column_length(table_name, table_data):
+    columns = {'s': 'bra_id_s', 'r': 'bra_id_r'}
+    cols_added = []
+    for index, column in columns.items():
+        if index in table_name:
+            table_data[column + "_len"] = table_data[column].str.len()
+            cols_added.append(column + "_len")
+    return table_data, cols_added
