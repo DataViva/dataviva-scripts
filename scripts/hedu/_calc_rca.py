04b921add276839a7c618636cd64817bf5486bfa
Jon Speiser <jspeis@gmail.com>
2015-04-15 09:30:48 -0400

    adding hedu scripts

---
+++ scripts/hedu/_calc_rca.py
@@ -0,0 +1,29 @@
+import sys, os
+import pandas as pd
+import numpy as np
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+growth_lib_path = os.path.abspath(os.path.join(file_path, "..", "growth_lib"))
+sys.path.insert(0, growth_lib_path)
+import growth
+
+def calc_rca(ybuc, year):
+    
+    ybc = ybuc.groupby(level=["year", "bra_id", "course_hedu_id"]).sum()
+    ybc = ybc[["enrolled"]]
+    ybc = ybc.reset_index()
+    ybc = ybc.drop("year", axis=1)
+            
+    rcas = ybc.pivot(index="bra_id", columns="course_hedu_id", values="enrolled")
+    rcas = growth.rca(rcas)
+    rcas = pd.DataFrame(rcas.stack(), columns=["enrolled_rca"])
+    
+    rcas = rcas.replace(0, np.nan)
+    rcas = rcas.dropna(how="all")
+        
+    rcas["year"] = int(year)
+    rcas = rcas.set_index("year", append=True)
+    rcas = rcas.swaplevel("year", "course_hedu_id")
+    rcas = rcas.swaplevel("year", "bra_id")
+    
+    return rcas
---
+++ scripts/hedu/format_raw_data.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+    Format Higher ED data for DB entry
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Columns:
+    0  Academic_organization
+    1  Municipality
+    2  ID_university
+    3  Year (2000 - 2012)
+    4  Adm_category
+    5  ID_course
+    6  Name_course
+    7  Modality
+    8  Level
+    9  Openings
+    10 Enrolled
+    11 Graduates
+    12 Entrants
+    13 Degree
+    
+    Example Usage
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    python scripts/higher_edu/format_raw_data.py data/higher_edu/undergraduate.csv -o -y data/higher_edu/
+
+"""
+
+''' Import statements '''
+import os, sys, time, bz2, click
+import pandas as pd
+import pandas.io.sql as sql
+import numpy as np
+
+from _to_df import to_df
+from _aggregate import aggregate
+from _column_lengths import add_column_length
+
+from _calc_rca import calc_rca
+
+
+def pre_check():
+    failed = []
+    for env_var in ["DATAVIVA_DB_USER", "DATAVIVA_DB_PW", "DATAVIVA_DB_NAME"]:
+        if os.environ.get(env_var) is None:
+            failed.append(env_var)
+    if len(failed):
+        sys.exit("The following environment variables need to be set: {0}".format(", ".join(failed)))
+
+@click.command()
+@click.argument('file_path', type=click.Path(exists=True))
+@click.option('-y', '--year', prompt='Year', help='year of the data to convert', required=True, default='all')
+@click.option('output_path', '--output', '-o', help='Path to save files to.', type=click.Path(), required=True, prompt="Output path")
+def main(file_path, year, output_path):
+    pre_check()
+    output_path = os.path.join(output_path, str(year))
+
+    print "\nYEAR: {0}\n".format(year)
+    this_output_path = os.path.join(output_path)
+    if not os.path.exists(this_output_path): os.makedirs(this_output_path)
+    
+    step = 0
+    step+=1; print '''STEP {0}: Import file to pandas dataframe'''.format(step)
+    df = to_df(file_path, year)
+    
+    tables_list = ["yb", "yu", "yc", "ybc", "ybu", "yuc", "ybuc"]
+    pk_lookup = {"y": "year", "b": "bra_id", "c": "course_hedu_id", "u": "university_id"}
+
+    ybuc = None
+
+    for table_name in tables_list:
+        pk = [pk_lookup[l] for l in table_name]
+        print "working on", table_name
+        
+        dems = ['gender', 'ethnicity', 'school_type'] if "d" in table_name else ['']
+        
+        for dem in dems:
+            print '''\nSTEP 2: Aggregate {0}'''.format(dem)
+            tbl = aggregate(pk, df, dem)
+            
+            if "c" in table_name:
+                pk2 = [x for x in pk]
+                pk2[pk2.index("course_hedu_id")] = df.course_hedu_id.str.slice(0, 2)
+                # df2.course_hedu_id = df.course_hedu_id.str.slice(0, 2)
+                tbl_course2 = aggregate(pk2, df, dem)
+
+                tbl = pd.concat([tbl, tbl_course2])
+            
+            tbl = add_column_length(table_name, tbl)
+            tbl.rename(columns={"student_id": "students"}, inplace=True)   
+            if table_name == "yb":
+                tbl.rename(columns={"university_id": "num_universities"}, inplace=True)   
+            if table_name == "ybuc":
+                print tbl.head()
+                ybuc = tbl
+            file_name = table_name + "_" + dem + ".tsv.bz2" if "d" in table_name else table_name + ".tsv.bz2"
+            print '''Save {0} to output path'''.format(file_name)
+            new_file_path = os.path.abspath(os.path.join(output_path, file_name))
+            tbl.to_csv(bz2.BZ2File(new_file_path, 'wb'), sep="\t", index=True)
+
+        # if "c" in table_name:
+        #     print '''\nSTEP 3: Aggregate {0}'''
+        #     tbl = aggregate(pk, df, '', 2)
+        #     tbl = add_column_length(table_name, tbl)
+        #     # print tbl.reset_index().course_hedu_id.nunique()
+        #     file_name = table_name + "_cid2.tsv.bz2"
+        #     print '''Save {0} to output path'''.format(file_name)
+        #     new_file_path = os.path.abspath(os.path.join(output_path, file_name))
+        #     tbl.to_csv(bz2.BZ2File(new_file_path, 'wb'), sep="\t", index=True)        
+    
+    if ybuc is not None:
+        step+=1; print '''STEP {0}: Calculating RCAs'''.format(step)
+        ybc = calc_rca(ybuc, year)
+        new_file_path = os.path.abspath(os.path.join(output_path, "ybc_rca.tsv.bz2"))
+        ybc.to_csv(bz2.BZ2File(new_file_path, 'wb'), sep="\t", index=True)
+        print "writing", new_file_path
+    
+
+if __name__ == "__main__":
+    main()
