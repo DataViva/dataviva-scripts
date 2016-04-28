2619956d7497de6df8ad432bf628ee5317cca2b0
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-28 02:29:36 -0300

    Convert identation to spaces

--- scripts/rais/_column_lengths.py
+++ scripts/rais/_column_lengths.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def add_column_length(table_name, table_data):
-	columns = {'b': 'bra_id', 'i': 'cnae_id', 'o': 'cbo_id'}
-	for index, column in columns.items():
-		if index in table_name:
+    columns = {'b': 'bra_id', 'i': 'cnae_id', 'o': 'cbo_id'}
+    for index, column in columns.items():
+        if index in table_name:
             if table_name == 'ybio':
                 table_data = table_data.set_index(["year", "bra_id", "cnae_id", "cbo_id"])
-			table_data[column + "_len"] = pd.Series( map(lambda x: len(str(x)), table_data.index.get_level_values(column)), index = table_data.index)
+            table_data[column + "_len"] = pd.Series( map(lambda x: len(str(x)), table_data.index.get_level_values(column)), index = table_data.index)
 
-	return table_data
+    return table_data
