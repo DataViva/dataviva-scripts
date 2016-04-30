4ee229012f76402af2b1c64206a5d65c38112ecd
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-30 16:29:26 -0300

    Pep 8

--- scripts/secex_monthly/_brazil_rca.py
+++ scripts/secex_monthly/_brazil_rca.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
+
 def brazil_rca(ymp, ypw_file_path, year):
 
-    bra_rcas = pd.read_csv(ypw_file_path, compression="bz2", sep="\t", converters={"hs_id":str})
+    bra_rcas = pd.read_csv(ypw_file_path, compression="bz2", sep="\t", converters={"hs_id": str})
     bra_rcas = bra_rcas[bra_rcas['wld_id'] == "sabra"]
 
     bra_rcas["month"] = "00"
