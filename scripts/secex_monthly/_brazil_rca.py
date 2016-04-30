d7f2f1a78e26039eeda9d799e7caca6f1935b47f
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-30 08:17:15 -0300

    Fixes null rca_wld issue

--- scripts/secex_monthly/_brazil_rca.py
+++ scripts/secex_monthly/_brazil_rca.py
@@ -6,8 +6,8 @@ def brazil_rca(ymp, ypw_file_path, year):
     bra_rcas = bra_rcas[bra_rcas['wld_id'] == "sabra"]
 
     bra_rcas["month"] = "00"
+    bra_rcas["year"] = int(year)
     bra_rcas = bra_rcas.set_index(["year", "month", "hs_id"])
-
     ymp["rca_wld"] = bra_rcas["rca"]
 
     return ymp
