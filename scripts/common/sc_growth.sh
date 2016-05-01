2766e800690bac4157f014c51f0390fbf39b95c8
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-01 19:12:39 -0300

    Fixes sc growth script

--- scripts/common/sc_growth.sh
+++ scripts/common/sc_growth.sh
@@ -10,5 +10,5 @@ YEAR=2015
 for i in $listOfNames
 do
 echo "$i\n";
-  python scripts/common/growth_calc.py data/sc/2015/$i $YEAR -c enroll_id -y 1,5 -e sc -o data/sc/growth_1_5_sc/$YEAR -s school_id
+  python -u scripts/common/growth_calc.py data/sc/$YEAR/$i data/sc/$YEAR/$i -c enroll_id -y $YEAR -e sc -o data/sc/growth_1_5_sc/$YEAR -s school_id &>> sc_growth.log
 done
