c218f5ccb7d934b92f249c2888d90dca21187d63
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-01 18:57:11 -0300

    Fix sc growth

--- scripts/common/sc_growth.sh
+++ scripts/common/sc_growth.sh
@@ -1,4 +1,4 @@
-listOfNames="yb.tsv.bz2 
+listOfNames="yb.tsv.bz2
 ybc.tsv.bz2
 ybs.tsv.bz2
 ybsc.tsv.bz2
@@ -6,9 +6,9 @@ yc.tsv.bz2
 ys.tsv.bz2
 ysc.tsv.bz2"
 
-YEAR=2012
+YEAR=2015
 for i in $listOfNames
 do
-#echo "$i\n";
-  python scripts/common/growth_calc.py data/sc/2012/$i -r $YEAR -c enroll_id -y 1,5 -e sc -o ~/growth_1_5_sc/$YEAR -s school_id
+echo "$i\n";
+  python scripts/common/growth_calc.py data/sc/2015/$i -r $YEAR -c enroll_id -y 1,5 -e sc -o ~/growth_1_5_sc/$YEAR -s school_id
 done
