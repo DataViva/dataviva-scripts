0ad560bd4bb36a44c736cc88057633f4f80b9c02
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-23 11:00:05 -0300

    Fixes aggregation for hedu

--- scripts/hedu/_aggregate.py
+++ scripts/hedu/_aggregate.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-def aggregate(this_pk, tbl):
-    tbl = tbl.drop(['gender', 'ethnicity', 'school_type'], axis=1)
+def aggregate(indexes, df):
+    df = df.drop(['gender', 'ethnicity', 'school_type'], axis=1)
 
     # -- For aggregation make sure we are only looking at the deepest level!!
-    deepestBra = tbl.bra_id.str.len() == 9
-    deepestCourse = tbl.course_hedu_id.str.len() == 6
+    deepestBra = df.bra_id.str.len() == 9
+    deepestCourse = df.course_hedu_id.str.len() == 6
 
     agg_rules = {
         "age": np.mean,
@@ -22,34 +22,41 @@ def aggregate(this_pk, tbl):
         "entrants": np.sum,
     }
 
-    pk_types = set([type(t) for t in this_pk])
-    if pk_types == set([str]) and this_pk == ["year", "bra_id"]:
+    pk_types = set([type(t) for t in indexes])
+    if pk_types == set([str]) and indexes == ["year", "bra_id"]:
         agg_rules["university_id"] = pd.Series.nunique
 
-    test = tbl[~(deepestBra & deepestCourse)]
-    tbl = tbl[deepestBra & deepestCourse]
+    test = df[~(deepestBra & deepestCourse)]
+    df = df[deepestBra & deepestCourse]
 
     if not test.empty:
-        print "ROWS REMOVED! On table", this_pk
+        print "ROWS REMOVED! On table", indexes
         print test.head()
     test = None
 
-    df_municipality = tbl.groupby(this_pk).agg(agg_rules)
+    aggregated_dfs = []
+    aggregated_dfs.append(df.groupby(indexes).agg(agg_rules))
 
-    df_region = tbl.reset_index()
-    df_region["bra_id"] = df_region["bra_id"].str.slice(0, 1)
-    df_region = df_region.groupby(this_pk).agg(agg_rules)
+    if 'course_hedu_id' in indexes:
+        df_fields = df.reset_index()
+        df_fields["course_hedu_id"] = df_fields["course_hedu_id"].str.slice(0, 1)
+        aggregated_dfs.append(df_fields.groupby(indexes).agg(agg_rules))
 
-    df_state = tbl.reset_index()
-    df_state["bra_id"] = df_state["bra_id"].str.slice(0, 3)
-    df_state = df_state.groupby(this_pk).agg(agg_rules)
+    if 'bra_id' in indexes:
+        df_region = df.reset_index()
+        df_region["bra_id"] = df_region["bra_id"].str.slice(0, 1)
+        aggregated_dfs.append(df_region.groupby(indexes).agg(agg_rules))
 
-    df_meso = tbl.reset_index()
-    df_meso["bra_id"] = df_meso["bra_id"].str.slice(0, 5)
-    df_meso = df_meso.groupby(this_pk).agg(agg_rules)
+        df_state = df.reset_index()
+        df_state["bra_id"] = df_state["bra_id"].str.slice(0, 3)
+        aggregated_dfs.append(df_state.groupby(indexes).agg(agg_rules))
 
-    df_micro = tbl.reset_index()
-    df_micro["bra_id"] = df_micro["bra_id"].str.slice(0, 7)
-    df_micro = df_micro.groupby(this_pk).agg(agg_rules)
+        df_meso = df.reset_index()
+        df_meso["bra_id"] = df_meso["bra_id"].str.slice(0, 5)
+        aggregated_dfs.append(df_meso.groupby(indexes).agg(agg_rules))
 
-    return pd.concat([df_municipality, df_state, df_meso, df_micro, df_region])
+        df_micro = df.reset_index()
+        df_micro["bra_id"] = df_micro["bra_id"].str.slice(0, 7)
+        aggregated_dfs.append(df_micro.groupby(indexes).agg(agg_rules))
+
+    return pd.concat(aggregated_dfs)
