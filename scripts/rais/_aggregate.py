860209b1fd73bc04b3814df3f55ad3311000794f
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-23 15:06:25 -0300

    pep 8

--- scripts/rais/_aggregate.py
+++ scripts/rais/_aggregate.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 import pandas as pd
 import numpy as np
 import itertools
@@ -15,13 +16,14 @@ def get_planning_regions():
     cursor = db.cursor()
 
     cursor.execute("select bra_id, pr_id from attrs_bra_pr")
-    return {r[0]:r[1] for r in cursor.fetchall()}
+    return {r[0]: r[1] for r in cursor.fetchall()}
+
 
 def aggregate_demographics(rais_df, bra_depths):
     rais_df['wage_med'] = rais_df['wage']
     rais_df['num_jobs'] = 1
 
-    rais_df = rais_df.rename(columns = {"age": "age_med", "literacy": "edu_mode"})
+    rais_df = rais_df.rename(columns={"age": "age_med", "literacy": "edu_mode"})
     rais_df = rais_df.drop(["color", "gender", "est_size"], axis=1)
 
     print "Aggregating demographic tables, from raw data..."
@@ -32,6 +34,7 @@ def aggregate_demographics(rais_df, bra_depths):
         dtbls[t_name] = table
     return dtbls
 
+
 def aggregate(rais_df, depths):
     bra_depths = depths["bra"]
     cnae_depths = depths["cnae"]
@@ -39,21 +42,21 @@ def aggregate(rais_df, depths):
     demo_depths = depths["demo"]
 
     pk = ["year", "bra_id", "cnae_id", "cbo_id"]
-    tables = {"ybio":["year", "bra_id", "cnae_id", "cbo_id"],
-                "ybi":["year", "bra_id", "cnae_id"],
-                "ybo":["year", "bra_id", "cbo_id"],
-                "yb":["year", "bra_id"],
-                "yi":["year", "cnae_id"],
-                "yio":["year", "cnae_id", "cbo_id"],
-                "yo":["year", "cbo_id"]}
+    tables = {"ybio": ["year", "bra_id", "cnae_id", "cbo_id"],
+              "ybi": ["year", "bra_id", "cnae_id"],
+              "ybo": ["year", "bra_id", "cbo_id"],
+              "yb": ["year", "bra_id"],
+              "yi": ["year", "cnae_id"],
+              "yio": ["year", "cnae_id", "cbo_id"],
+              "yo": ["year", "cbo_id"]}
     import time
     s = time.time()
 
     agg_rules = {
         "wage": np.sum,
         "num_jobs": np.sum,
-        "num_emp" : pd.Series.nunique,
-        "est_id" : lambda est_id: set.union(set(est_id)),
+        "num_emp": pd.Series.nunique,
+        "est_id": lambda est_id: set.union(set(est_id)),
         "age": np.sum
     }
 
@@ -110,20 +113,17 @@ def aggregate(rais_df, depths):
                 ybio_new_depths = pd.concat([ybio_new_depths, ybio_depth])
             tables[t_name] = pd.concat([ybio_new_depths, tables[t_name]])
 
-
-
         # tables[t_name]["num_emp"] = tables[t_name]["num_emp"].apply(lambda x: len(x))
         tables[t_name]["est_id"] = tables[t_name]["est_id"].apply(lambda x: len(x))
         tables[t_name]["wage_avg"] = tables[t_name]["wage"] / tables[t_name]["num_jobs"]
         tables[t_name]["age"] = tables[t_name]["age"] / tables[t_name]["num_jobs"]
-        tables[t_name] = tables[t_name].rename(columns={"age":"age_avg","est_id":"num_est"})
+        tables[t_name] = tables[t_name].rename(columns={"age": "age_avg", "est_id": "num_est"})
 
-        print "Is unique:",tables[t_name].index.is_unique
+        print "Is unique:", tables[t_name].index.is_unique
 
         # tables[t_name].to_csv(t_name+".csv")
-        print "time:",(time.time() - ss) / 60
+        print "time:", (time.time() - ss) / 60
 
-    print "total aggregation time:",(time.time() - s) / 60
+    print "total aggregation time:", (time.time() - s) / 60
 
     return tables
-
--- scripts/sc/format_raw_data.py
+++ scripts/sc/format_raw_data.py
@@ -19,7 +19,7 @@
     13: Municipality
     14: Location
     15: Adm_Dependency
-    
+
     Example Usage
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     python scripts/edu/format_raw_data.py data/edu/School_census_2007.csv.bz2 -y 2007 -o data/edu/
@@ -27,17 +27,22 @@
 """
 
 ''' Import statements '''
-import os, sys, time, bz2, click
+import os
+import sys
+import time
+import bz2
+import click
 import pandas as pd
 import pandas.io.sql as sql
 import numpy as np
 
 from _to_df import to_df
 from _replace_vals import replace_vals
-from _aggregate import aggregate
+from _aggregate import aggregate, agg_rules
 from _shard import shard
 from _calc_rca import calc_rca
 from _column_lengths import add_column_length
+
 
 def pre_check():
     failed = []
@@ -47,6 +52,7 @@ def pre_check():
     if len(failed):
         sys.exit("The following environment variables need to be set: {0}".format(", ".join(failed)))
 
+
 @click.command()
 @click.argument('file_path', type=click.Path(exists=True))
 @click.option('-y', '--year', prompt='Year', help='year of the data to convert', required=True)
@@ -54,12 +60,14 @@ def pre_check():
 def main(file_path, year, output_path):
     pre_check()
     output_path = os.path.join(output_path)
-    
-    if not os.path.exists(output_path): os.makedirs(output_path)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     print "Output Path=", output_path
-    d = pd.HDFStore(os.path.abspath(os.path.join(output_path,'sc_data.h5')))
-    print; print '''STEP 1: \nImport file to pandas dataframe'''
-    
+    d = pd.HDFStore(os.path.abspath(os.path.join(output_path, 'sc_data.h5')))
+    print
+    print '''STEP 1: \nImport file to pandas dataframe'''
+
     hdf_filepath = output_path + "/store_df.h5"
 
     print "LOOKING for HDF file at location ", hdf_filepath
@@ -73,33 +81,32 @@ def main(file_path, year, output_path):
         print "SAVING HDF to", hdf_filepath
         df.to_hdf(hdf_filepath, 'table')
 
-    print; print "Step 2: aggregate"
-    from _aggregate import agg_rules
-
+    print
+    print "Step 2: aggregate"
 
     pk_lookup = {"y": "year", "b": "bra_id", "c": "course_sc_id", "s": "school_id"}
-    
+
     tables_list = ["yb", "yc", "ys", "ybs", "ybc", "ysc", "ybsc"]
 
     for table_name in tables_list:
         pk = [pk_lookup[l] for l in table_name]
         print "working on", table_name
-        
+
         dems = ['gender', 'color', 'loc', 'school_type'] if "d" in table_name else ['']
-        
+
         for dem in dems:
             print '''\nSTEP 2: Aggregate {0}'''.format(dem)
             tbl = aggregate(table_name, pk, df, dem)
-            
+
             if "c" in table_name:
                 pk2 = [x for x in pk]
                 pk2[pk2.index("course_sc_id")] = df.course_sc_id.str.slice(0, 2)
                 tbl_course2 = aggregate(table_name, pk2, df, dem, course_flag=True)
 
                 tbl = pd.concat([tbl, tbl_course2])
-            
+
             tbl = add_column_length(table_name, tbl)
-            # tbl.rename(columns={"student_id": "students"}, inplace=True)   
+            # tbl.rename(columns={"student_id": "students"}, inplace=True)
             file_name = table_name + "_" + dem + ".tsv.bz2" if "d" in table_name else table_name + ".tsv.bz2"
             print '''Save {0} to output path'''.format(file_name)
             new_file_path = os.path.abspath(os.path.join(output_path, file_name))
