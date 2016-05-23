395ddc56727f39a79fd78501cb87069ddc909b65
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-23 15:09:11 -0300

    pep 8

--- scripts/sc/_aggregate.py
+++ scripts/sc/_aggregate.py
@@ -1,30 +1,34 @@
-import sys, MySQLdb, os
+import sys
+import MySQLdb
+import os
 import pandas as pd
 import numpy as np
 import re
 
 agg_rules = {
-    "age" : np.mean,
+    "age": np.mean,
     "enroll_id": pd.Series.count,
     "class_id": pd.Series.nunique,
-    "distorted_age" : np.sum,
+    "distorted_age": np.sum,
 }
 
 commute_rules = {
-    "commute_distance" : np.mean
+    "commute_distance": np.mean
 }
 
 # pk = ["year", "bra_id", "school_id", "course_sc_id", "d_id"]
 
-db = MySQLdb.connect(host=os.environ.get("DATAVIVA_DB_HOST", "localhost"), user=os.environ["DATAVIVA_DB_USER"], passwd=os.environ["DATAVIVA_DB_PW"], db=os.environ["DATAVIVA_DB_NAME"])
+db = MySQLdb.connect(host=os.environ.get("DATAVIVA_DB_HOST", "localhost"), user=os.environ[
+                     "DATAVIVA_DB_USER"], passwd=os.environ["DATAVIVA_DB_PW"], db=os.environ["DATAVIVA_DB_NAME"])
 db.autocommit(1)
-distances = {} 
+distances = {}
+
 
 def get_planning_regions():
     ''' Connect to DB '''
     cursor = db.cursor()
     cursor.execute("select bra_id, pr_id from attrs_bra_pr")
-    return {r[0]:r[1] for r in cursor.fetchall()}
+    return {r[0]: r[1] for r in cursor.fetchall()}
 
 planning_regions = get_planning_regions()
 
@@ -36,22 +40,21 @@ def aggregate(table_name, this_pk, tbl, dem, cid_len=None, course_flag=None):
         # this_pk = pk
     # else:
         # this_pk = pk[:-1]
-    
+
     # print "getting distances..."
     if table_name != "yb":
         this_agg_rules = agg_rules
     else:
         this_agg_rules = {
-            "age" : np.mean,
+            "age": np.mean,
             "enroll_id": pd.Series.count,
             "class_id": pd.Series.nunique,
-            "distorted_age" : np.sum,
-            "school_id" : pd.Series.nunique,
+            "distorted_age": np.sum,
+            "school_id": pd.Series.nunique,
         }
 
-
     # tbl = tbl.drop(['gender', 'color', 'loc', 'school_type'], axis=1)
-    
+
     if cid_len:
         tbl['course_sc_id'] = tbl["course_sc_id"].str.slice(0, cid_len)
 
@@ -63,7 +66,7 @@ def aggregate(table_name, this_pk, tbl, dem, cid_len=None, course_flag=None):
     tbl_region = tbl.reset_index()
     tbl_region["bra_id"] = tbl_region["bra_id"].str.slice(0, 1)
     tbl_region = tbl_region.groupby(this_pk).agg(this_agg_rules)
-    
+
     print "Step C."
     tbl_state = tbl.reset_index()
     tbl_state["bra_id"] = tbl_state["bra_id"].str.slice(0, 3)
@@ -85,14 +88,12 @@ def aggregate(table_name, this_pk, tbl, dem, cid_len=None, course_flag=None):
     tbl_pr["bra_id"] = tbl_pr["bra_id"].astype(str).replace(planning_regions)
     tbl_pr = tbl_pr.groupby(this_pk).agg(this_agg_rules)
 
-
     master_table = pd.concat([tbl_all, tbl_state, tbl_meso, tbl_micro, tbl_pr, tbl_region])
 
     if cid_len or course_flag:
         print "Step G. (course_sc_id step) compute distortion rate"
         master_table['distortion_rate'] = master_table["distorted_age"] / master_table["enroll_id"]
-        master_table.loc[master_table['distorted_age'].isnull() , 'distortion_rate'] = '\N'
-    
+        master_table.loc[master_table['distorted_age'].isnull(), 'distortion_rate'] = '\N'
+
     master_table.drop('distorted_age', axis=1, inplace=True)
     return master_table
-
