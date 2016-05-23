c005c9e4e90cfed90cf9f074fdfe201b82700cd2
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-23 09:32:22 -0300

    Removes planning regions from hedu aggregate

--- scripts/hedu/_aggregate.py
+++ scripts/hedu/_aggregate.py
@@ -8,14 +8,14 @@ def get_planning_regions():
     db = MySQLdb.connect(host=os.environ["DATAVIVA_DB_HOST"], user=os.environ["DATAVIVA_DB_USER"], passwd=os.environ["DATAVIVA_DB_PW"], db=os.environ["DATAVIVA_DB_NAME"])
     db.autocommit(1)
     cursor = db.cursor()
-    
+
     cursor.execute("select bra_id, pr_id from attrs_bra_pr")
     return {r[0]:r[1] for r in cursor.fetchall()}
 
 def aggregate(this_pk, tbl, dem):
     if dem:
         tbl["d_id"] = tbl[dem]
-    
+
     tbl = tbl.drop(['gender', 'ethnicity', 'school_type'], axis=1)
 
     # -- For aggregation make sure we are only looking at the deepest level!!
@@ -34,7 +34,7 @@ def aggregate(this_pk, tbl, dem):
         "full_time": np.sum,
         "entrants": np.sum,
     }
-    
+
     pk_types = set([type(t) for t in this_pk])
     if pk_types == set([str]) and this_pk == ["year", "bra_id"]:
         agg_rules["university_id"] = pd.Series.nunique
@@ -43,7 +43,7 @@ def aggregate(this_pk, tbl, dem):
     tbl = tbl[ deepestBra & deepestCourse ]
 
     if not test.empty:
-        print "ROWS REMOVED! On table", this_pk 
+        print "ROWS REMOVED! On table", this_pk
         print test.head()
     test = None
 
@@ -52,7 +52,7 @@ def aggregate(this_pk, tbl, dem):
     tbl_region = tbl.reset_index()
     tbl_region["bra_id"] = tbl_region["bra_id"].str.slice(0, 1)
     tbl_region = tbl_region.groupby(this_pk).agg(agg_rules)
-    
+
     tbl_state = tbl.reset_index()
     tbl_state["bra_id"] = tbl_state["bra_id"].str.slice(0, 3)
     tbl_state = tbl_state.groupby(this_pk).agg(agg_rules)
@@ -65,9 +65,4 @@ def aggregate(this_pk, tbl, dem):
     tbl_micro["bra_id"] = tbl_micro["bra_id"].str.slice(0, 7)
     tbl_micro = tbl_micro.groupby(this_pk).agg(agg_rules)
 
-    tbl_pr = tbl.reset_index()
-    tbl_pr = tbl_pr[tbl_pr["bra_id"].map(lambda x: x[:3] == "4mg")]
-    tbl_pr["bra_id"] = tbl_pr["bra_id"].astype(str).replace(get_planning_regions())
-    tbl_pr = tbl_pr.groupby(this_pk).agg(agg_rules)
-
-    return pd.concat([tbl_all, tbl_state, tbl_meso, tbl_micro, tbl_pr, tbl_region])    
+    return pd.concat([tbl_all, tbl_state, tbl_meso, tbl_micro, tbl_pr, tbl_region])
