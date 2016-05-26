5884d6c08b84f274fe5bd51c2874ff5256cb6302
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-26 09:57:48 -0300

    pep 8

--- scripts/sc/_calc_rca.py
+++ scripts/sc/_calc_rca.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 import pandas as pd
 import numpy as np
 
@@ -7,36 +8,37 @@ ps_calcs_lib_path = os.path.abspath(os.path.join(file_path, "../../", "lib/ps_ca
 sys.path.insert(0, ps_calcs_lib_path)
 import ps_calcs
 
+
 def calc_rca(ybc, year):
-    
+
     rcas = pd.DataFrame()
     for geo_level in [2, 4, 8]:
-        
+
         print "geo level:", geo_level
-        
+
         ybc_data = ybc.reset_index()
-        
+
         bra_criterion = ybc_data["bra_id"].map(lambda x: len(x) == geo_level)
         course_criterion = ybc_data["course_sc_id"].map(lambda x: len(x) == 5)
         ybc_data = ybc_data[bra_criterion & course_criterion]
-        
+
         ybc_data = ybc_data[["bra_id", "course_sc_id", "students"]]
-        
+
         ybc_data = ybc_data.pivot(index="bra_id", columns="course_sc_id", values="students")
         ybc_data_rca = ps_calcs.rca(ybc_data)
         ybc_data_rca = pd.DataFrame(ybc_data_rca.stack(), columns=["students_rca"])
-        
+
         if rcas.empty:
             rcas = ybc_data_rca
         else:
             rcas = pd.concat([rcas, ybc_data_rca])
         rcas = rcas.replace(0, np.nan)
         rcas = rcas.dropna(how="all")
-        
+
     rcas["year"] = int(year)
     rcas = rcas.set_index("year", append=True)
     rcas = rcas.swaplevel("year", "course_sc_id")
     rcas = rcas.swaplevel("year", "bra_id")
     ybc = ybc.merge(rcas, how="outer", left_index=True, right_index=True)
-    
+
     return ybc
